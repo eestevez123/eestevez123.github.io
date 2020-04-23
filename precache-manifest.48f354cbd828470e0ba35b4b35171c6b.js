a1458dda67d9cd3235e682ee8c0f6fbddc8f85fa
Eddie Estevez <eestevez@wisc.edu>
2020-04-22 23:03:50 -0500

    hope this works

---
+++ precache-manifest.48f354cbd828470e0ba35b4b35171c6b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b866777938592217a3ac23e76e63a7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82c2901528ef96274412",
+    "url": "/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "0c5855855a88bcbbc301",
+    "url": "/static/css/main.ec8eef84.chunk.css"
+  },
+  {
+    "revision": "82c2901528ef96274412",
+    "url": "/static/js/2.b161af66.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b161af66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c5855855a88bcbbc301",
+    "url": "/static/js/main.6bc8fd8d.chunk.js"
+  },
+  {
+    "revision": "d83b0bfa60d063e14f96",
+    "url": "/static/js/runtime-main.656ac742.js"
+  },
+  {
+    "revision": "5846fabba2c6320493808d2d1acdc459",
+    "url": "/static/media/EddieEstevez.5846fabb.jpg"
+  }
+]);
